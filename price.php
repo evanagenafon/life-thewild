0985b90f77ccaf43469d1e50f51d57314250957e
金澤優 <kanazawayuu@kanazawayuu-no-MacBook-Pro.local>
2016-08-26 14:55:01 +0900

    check

---
+++ price.php
@@ -0,0 +1,43 @@
+<?php
+/*
+* Template Name: 料金表
+*/
+?>
+<?PHP get_header(); ?>
+<h1>ホームページ制作料金表</h1>
+<table>
+	<tr>
+		<th>メニュー</th><th>金額</th>
+	</tr>
+	<tr>
+		<td>制作費用(TOP＋4ページ)</td><td>80,000円</td>
+	</tr>
+	<tr>
+		<td>追加ページ</td><td>12,000円</td>
+	</tr>
+	<tr>
+		<td>レスポンシブ対応</td><td>40,000円</td>
+	</tr>
+	<tr>
+		<td>wordpress導入</td><td>50,000円</td>
+	</tr>
+	<tr>
+		<td>SNS連動</td><td>7,000円</td>
+	</tr>
+	<tr>
+		<td>バナー制作</td><td>5,000円</td>
+	</tr>
+	<tr>
+		<td>ロゴ制作</td><td>15,000円</td>
+	</tr>
+	<tr>
+		<td>サーバー管理費・更新費</td><td>6,000円/月</td>
+	</tr>
+</table>
+</div>
+<footer>
+<div class="footer-wrap">
+<address>Copyright © <a href="http://life-thewild.com/">THE WILD 〜行動しない天才〜</a> ALL RIGHTS RESERVED.</address>
+</div>
+</footer>
+</body>
