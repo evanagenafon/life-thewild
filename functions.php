3fd5e10f5ff09275a6d3ab4dbedfde5fb6c45690
金澤優 <kanazawayuu@kanazawayuu-no-MacBook-Pro.local>
2016-09-05 20:52:59 +0900

    thumnale

--- functions.php
+++ functions.php
@@ -9,3 +9,18 @@ function include_parent_skins(){
 add_editor_style();
 
 //以下にSimplicity子テーマ用の関数を書く
+
+//投稿1枚目の画像をサムネイルに
+function catch_that_image() {
+global $post, $posts;
+$first_img = '';
+ob_start();
+ob_end_clean();
+$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/', $post->post_content, $matches);
+$first_img = $matches [1][0];
+
+if(empty($first_img)){ //Defines a default image
+	$first_img = "http://life-thewild.com/wp-content/uploads/2016/08/business-561387_1280.jpg";
+}
+return $first_img;
+}
